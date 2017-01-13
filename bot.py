00b01965c619524174de1537ec74cb0be6e827bf
Eli Tabello <etabw3@gmail.com>
2017-01-12 23:11:00 -0500

    todo

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from mentions import Mentions
 from sendtweet import SendTweet
+from tokenizetweet import TokenizeTweet
 from login import Login
 from red import RedSet
 import sys
@@ -11,16 +12,23 @@ creds = twitterlogin.api
 tweeter = SendTweet(creds)
 
 redisdb = RedSet()
-
 lastid = redisdb.getlastid()
 
 m = Mentions(creds)
 mention_list = m.mentions(lastid)
 
+tokenizer = TokenizeTweet()
+
 for m in mention_list:
-  tweet = 'hello @{}, thanks for the mention...'.format(m.user)
+  u = m.user.lstrip('@')
+  tweet = 'hello @{}, thanks for the mention...'.format(u)
   
   if not redisdb.inset(m.id):
+
+    
+    # query = tokenizer.tokenize(m.text)
+    # get results from web service
+    # form response tweet
 
     return_code = tweeter.tweet(tweet, m.id)
 
--- sendtweet.py
+++ sendtweet.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-
 from twitter import TwitterError
 
 class SendTweet:
 
   def __init__(self, creds):
     self.T = creds
+    self.MAX = 140
 
   def tweet(self, text, inreplyto):
     try:
-      self.T.PostUpdate(text, in_reply_to_status_id=inreplyto)
+      truncated = text[:self.MAX]
+      self.T.PostUpdate(truncated, in_reply_to_status_id=inreplyto)
       return 200
     except TwitterError as twe:
       return 187
